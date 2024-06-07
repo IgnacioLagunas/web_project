accbca4172881e35320c646fc2b461475d674479
Panamaxi <maximilianoduarte824@gmail.com>
2024-06-07 18:39:20 -0400

    arreglos2

--- Django/Django/urls.py
+++ Django/Django/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('ZonaGamers',include("ZonaGamers.urls"))
+    path('ZonaGamers',include("ZonaGamers.urls")),
+    path('',include("ZonaGamers.urls"))
 ]
